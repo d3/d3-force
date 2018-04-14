f2486043534474b31d768631774fb19709030c2f
John Alexis Guerra Gómez <john.guerra@gmail.com>
2018-04-14 18:29:34 -0500

    Removing visualization of arrows

--- src/boundary.js
+++ src/boundary.js
@@ -103,81 +103,9 @@ export default function(x0, y0, x1, y1) {
     return arguments.length ? (border = typeof _ === "function" ? _ : constant(+_), initialize(), force) : border;
   };
 
-
   force.hardBoundary = function(_) {
     return arguments.length ? (hardBoundary = _, force) : hardBoundary;
   };
-
-  force.visualizeStrength = function(ctx, _alpha) {
-    var x, y, vx, vy, step=20,
-      line,
-      halfX = x0() + (x1() - x0())/2,
-      halfY = y0() + (y1() - y0())/2,
-      alpha = _alpha !== undefined ? _alpha : 0.7;
-
-    if (!borderz) return;
-
-    ctx.save();
-    ctx.globalAlpha= 0.3;
-
-
-    for (x = x0(); x < x1() ; x += step) {
-      for (y  = y0(); y <y1() ; y += step) {
-        if ((x < (x0() + borderz[0]) || x > (x1() - borderz[0])) ||
-          (y < (y0() + borderz[0]) || y > (y1() - borderz[0])) ) {
-          vx = getVx(halfX, x, strength(), borderz[0], alpha);
-          vy = getVx(halfY, y, strength(), borderz[0], alpha);
-          line = new Line(x, y, x+vx, y+vy);
-          line.drawWithArrowheads(ctx);
-
-        }
-      }
-    }
-    ctx.restore();
-  }
-
-  function Line(x1,y1,x2,y2){
-      this.x1=x1;
-      this.y1=y1;
-      this.x2=x2;
-      this.y2=y2;
-  }
-  Line.prototype.drawWithArrowheads=function(ctx){
-
-      // arbitrary styling
-      ctx.strokeStyle="steelblue";
-      ctx.fillStyle="steelblue";
-      ctx.lineWidth=1;
-
-      // draw the line
-      ctx.beginPath();
-      ctx.moveTo(this.x1,this.y1);
-      ctx.lineTo(this.x2,this.y2);
-      ctx.stroke();
-
-      // // draw the starting arrowhead
-      // var startRadians=Math.atan((this.y2-this.y1)/(this.x2-this.x1));
-      // startRadians+=((this.x2>this.x1)?-90:90)*Math.PI/180;
-      // this.drawArrowhead(ctx,this.x1,this.y1,startRadians);
-      // draw the ending arrowhead
-      var endRadians=Math.atan((this.y2-this.y1)/(this.x2-this.x1));
-      endRadians+=((this.x2 >= this.x1)?90:-90)*Math.PI/180;
-      this.drawArrowhead(ctx,this.x2,this.y2,endRadians);
-
-
-  }
-  Line.prototype.drawArrowhead=function(ctx,x,y,radians){
-      ctx.save();
-      ctx.beginPath();
-      ctx.translate(x,y);
-      ctx.rotate(radians);
-      ctx.moveTo(0,0);
-      ctx.lineTo(2,5);
-      ctx.lineTo(-2,5);
-      ctx.closePath();
-      ctx.restore();
-      ctx.fill();
-  }
 
   return force;
 }
