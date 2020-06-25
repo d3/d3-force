a72cd1f4a2987654752e1899e8e9da18d36c7db7
Philippe Rivière <fil@rezo.net>
2020-06-25 17:14:07 +0200

    deterministic prng fixes #121.(maybe)
    do we need to expose jiggle(seed) so people could set the seed? (I don't think so)

--- src/jiggle.js
+++ src/jiggle.js
@@ -1,3 +1,8 @@
-export default function() {
-  return (Math.random() - 0.5) * 1e-6;
+// https://en.wikipedia.org/wiki/Linear_congruential_generator#Parameters_in_common_use
+const a = 1664525,
+      c = 1013904223,
+      m = 4294967296;
+let s = 1;
+export default function(seed) {
+  return (s = seed || (a * s + c) % m) / m * 1e-6;
 }
