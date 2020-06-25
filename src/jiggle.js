f3bb4780797368c79e79b9cc2fb77b2cfadc3fef
Philippe Rivière <fil@rezo.net>
2020-06-25 17:14:07 +0200

    deterministic prng fixes #121 thanks, @benstevens48!

--- src/jiggle.js
+++ src/jiggle.js
@@ -1,3 +1,9 @@
-export default function() {
-  return (Math.random() - 0.5) * 1e-6;
+// https://en.wikipedia.org/wiki/Linear_congruential_generator#Parameters_in_common_use
+const a = 1664525,
+      c = 1013904223,
+      m = 4294967296;
+let s = 1;
+export default function(seed) {
+  if (seed) s = Math.abs(a * seed);
+  return ((s = (a * s + c) % m) / m - 0.5) * 1e-6;
 }
