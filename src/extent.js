867fb4337ee60e3fc134035e6fe9d76aae6c164e
Philippe Rivière <fil@rezo.net>
2020-06-29 09:06:37 +0200

    fix bottom bound

--- src/extent.js
+++ src/extent.js
@@ -7,7 +7,7 @@ export default function(extent) {
     for (var i = 0; i < nodes.length; ++i) {
       var node = nodes[i], r = node.radius || 0;
       node.x = clamp(node.x, extent[0][0] - r, extent[1][0] + r);
-      node.y = clamp(node.y, extent[0][1] - r, extent[1][0] + r);
+      node.y = clamp(node.y, extent[0][1] - r, extent[1][1] + r);
     }
   }
 
