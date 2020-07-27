006ea1bc2f3fb7c41d09c2d845049104a86fd007
Philippe Rivière <fil@rezo.net>
2020-07-27 12:33:34 +0200

    forceCenter is now more gentle

--- test/center-test.js
+++ test/center-test.js
@@ -8,7 +8,7 @@ tape("forceCenter repositions nodes", function(test) {
   const f = force.forceSimulation().force("center", center).stop();
   const a = { x: 100, y: 0 }, b = { x: 200, y: 0 }, c = { x: 300, y: 0 };
   f.nodes([a, b, c]);
-  f.tick();
+  f.alphaDecay(0).tick(250);
   test.nodeEqual(a, { index: 0, x: -100, y: 0, vy: 0, vx: 0 });
   test.nodeEqual(b, { index: 1, x: 0, y: 0, vy: 0, vx: 0 });
   test.nodeEqual(c, { index: 2, x: 100, y: 0, vy: 0, vx: 0 });
