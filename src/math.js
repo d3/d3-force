0c436b2620e21277ae48b895a8818a699ebc042b
Philippe Rivière <fil@rezo.net>
2020-08-01 12:41:30 +0200

    forceRadial().angle() sets the preferred angle
    as in https://observablehq.com/@fil/forcepolar
    closes #152

---
+++ src/math.js
@@ -0,0 +1,2 @@
+export var pi = Math.PI;
+export var radians = pi / 180;
--- src/radial.js
+++ src/radial.js
@@ -1,26 +1,52 @@
 import constant from "./constant.js";
+import {radians} from "./math.js";
 
-export default function(radius, x, y) {
+function value(x) {
+  if (typeof x === "function") return x;
+  if (x === null || x === undefined || isNaN(x = +x)) return;
+  return constant(x);
+}
+
+export default function(radius, x, y, angle) {
   var nodes,
       strength = constant(0.1),
       strengths,
-      radiuses,
+      radii,
       xs,
-      ys;
+      ys,
+      angles;
 
-  if (typeof radius !== "function") radius = constant(+radius);
-  if (typeof x !== "function") x = constant(x == null ? 0 : +x);
-  if (typeof y !== "function") y = constant(y == null ? 0 : +y);
+  radius = value(radius);
+  x = value(x) || constant(0);
+  y = value(y) || constant(0);
+  angle = value(angle);
 
   function force(alpha) {
     for (var i = 0, n = nodes.length; i < n; ++i) {
       var node = nodes[i],
           dx = node.x - xs[i] || 1e-6,
           dy = node.y - ys[i] || 1e-6,
-          r = Math.sqrt(dx * dx + dy * dy),
-          k = (radiuses[i] - r) * strengths[i] * alpha / r;
-      node.vx += dx * k;
-      node.vy += dy * k;
+          r = Math.sqrt(dx * dx + dy * dy);
+
+      if (radius) {
+        var k = ((radii[i] - r) * strengths[i] * alpha) / r;
+        node.vx += dx * k;
+        node.vy += dy * k;
+      }
+
+      if (angle) {
+        var a = Math.atan2(dy, dx),
+          diff = angles[i] - a,
+          q = r * Math.sin(diff) * (strengths[i] * alpha);
+
+        // the factor below augments the "unease" for points that are opposite
+        // the correct direction: in that case, though sin(diff) is small,
+        // tan(diff/2) is very high
+        q *= Math.hypot(1, Math.tan(diff / 2));
+
+        node.vx += -q * Math.sin(a);
+        node.vy += q * Math.cos(a);
+      }
     }
   }
 
@@ -28,14 +54,16 @@ export default function(radius, x, y) {
     if (!nodes) return;
     var i, n = nodes.length;
     strengths = new Array(n);
-    radiuses = new Array(n);
+    radii = new Array(n);
     xs = new Array(n);
     ys = new Array(n);
+    angles = new Array(n);
     for (i = 0; i < n; ++i) {
-      radiuses[i] = +radius(nodes[i], i, nodes);
+      if (radius) radii[i] = +radius(nodes[i], i, nodes);
       xs[i] = +x(nodes[i], i, nodes);
       ys[i] = +y(nodes[i], i, nodes);
-      strengths[i] = isNaN(radiuses[i]) ? 0 : +strength(nodes[i], i, nodes);
+      if (angle) angles[i] = +angle(nodes[i], i, nodes) * radians;
+      strengths[i] = isNaN(radii[i]) ? 0 : +strength(nodes[i], i, nodes);
     }
   }
 
@@ -44,19 +72,23 @@ export default function(radius, x, y) {
   };
 
   force.strength = function(_) {
-    return arguments.length ? (strength = typeof _ === "function" ? _ : constant(+_), initialize(), force) : strength;
+    return arguments.length ? (strength = value(_) || constant(1), initialize(), force) : strength;
   };
 
   force.radius = function(_) {
-    return arguments.length ? (radius = typeof _ === "function" ? _ : constant(+_), initialize(), force) : radius;
+    return arguments.length ? (radius = value(_), initialize(), force) : radius;
   };
 
   force.x = function(_) {
-    return arguments.length ? (x = typeof _ === "function" ? _ : constant(+_), initialize(), force) : x;
+    return arguments.length ? (x = value(_) || constant(0), initialize(), force) : x;
   };
 
   force.y = function(_) {
-    return arguments.length ? (y = typeof _ === "function" ? _ : constant(+_), initialize(), force) : y;
+    return arguments.length ? (y = value(_) || constant(0), initialize(), force) : y;
+  };
+
+  force.angle = function(_) {
+    return arguments.length ? (angle = value(_), initialize(), force) : y;
   };
 
   return force;
